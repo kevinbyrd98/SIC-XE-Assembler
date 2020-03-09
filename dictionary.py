d96e4c2f3c33cc3236c3700be8157d24717b654c
kevinbyrd98 <30849649+kevinbyrd98@users.noreply.github.com>
2020-03-09 19:52:22 -0400

    Delete dictionary.py
    Removed code not made by me personally

--- dictionary.py
+++
@@ -1,6 +0,0 @@
-#made by Jon Lemos 2/11/2019
-thing = [s.split() for s in open('juice.txt', 'r')]
-print("{")
-for t in thing:
-    print("\t\"" + t[0] + "\": [0x" + t[1] + ", " + t[2] + "],")
-print("}")
